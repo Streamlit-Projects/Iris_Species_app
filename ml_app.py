2137b0e3634f9eef8e0c7222772cd7061f0a758c
mBohunickaCharles <105567611+mBohunickaCharles@users.noreply.github.com>
2022-06-19 18:21:15 +0100

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -5,25 +5,24 @@ import joblib
 from PIL import Image
 import os.path
 
+dir_name = os.path.abspath(os.path.dirname(__file__))
 
 def load_model(model_file):
-    dir_ml = os.path.abspath(os.path.ml_models(__file__))
-    loaded_model = joblib.load(open(os.path.join(dir_ml, model_file),"rb"))
+    loaded_model = joblib.load(open(os.path.join('ml_models', model_file),"rb"))
     return loaded_model
 
 
 def run_ml_app():
     st.subheader("Machine Learning Section")
-    
+
     # Loading our pretrained knn model:
     knn_model = load_model("knn_model.pkl")
-    
+
     # Load iris images:
-    dir_iris = os.path.abspath(os.path.iris_pictures(__file__))
-    setosa = Image.open(os.path.join(dir_iris, "i_setosa.jpg"))
-    versicolor = Image.open(os.path.join(dir_iris, "i_versicolor.jpg"))
-    virginica = Image.open(os.path.join(dir_iris, "i_virginica.jpg"))
-    
+    setosa = Image.open(os.path.join('iris_pictures', "i_setosa.jpg"))
+    versicolor = Image.open(os.path.join('iris_pictures', "i_versicolor.jpg"))
+    virginica = Image.open(os.path.join('iris_pictures', "i_virginica.jpg"))
+
     st.sidebar.markdown("#### Select Features:")
     parameter_list = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width']
     parameter_input = []
@@ -32,13 +31,13 @@ def run_ml_app():
     for parameter, parameter_df in zip(parameter_list, parameter_default):
         values =st.sidebar.slider(label = parameter, key=parameter, value = float(parameter_df), min_value = 0.0, max_value = 8.0, step = 0.1)
         parameter_input.append(values)
-        
+
     input_variables = pd.DataFrame([parameter_input], columns=parameter_list, dtype = float)
     st.write('\n\n')
-    
+
     prediction = knn_model.predict(input_variables)
     prd = ' '.join(prediction)
-    
+
     if st.button("Click Here to Classify"):
         st.write(prd)
         if prd == 'Iris-setosa':
